d09c1146b65f05b65c055084079cd0501a68c48e
rushanatownend <volen21@yahoo.com>
2024-05-07 15:31:12 +0100

    Add dotenv-rails

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -2,30 +2,24 @@ class BagsController < ApplicationController
   def index
     @bags = Bag.all
   end
-
   def show
     @bag = Bag.find(params[:id])
   end
-
   def new
     @bag = Bag.new
   end
-
   def create
-    @bag = Bag.find(params[:id])
     @bag = Bag.new(bag_params)
+    @bag.user = current_user
     if @bag.save
-      redirect_to bag_path(@bag), notice: "You new bag post is sucessful!!"
+      redirect_to bag_path(@bag), notice: "Your new bag post is sucessful!!"
     else
       render :new, status: :unprocessable_entity
     end
   end
-
   def destroy
   end
-
   private
-
   def bag_params
     params.require(:bag).permit(:name, :brand, :price, :description, :picture_url, :condition, :location)
   end
