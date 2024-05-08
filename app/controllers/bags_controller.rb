648950653ecb44cd027c75877ebb186669a65a7e
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-05-08 23:39:23 +0100

    navbar links, edit and delete bag working

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -2,12 +2,15 @@ class BagsController < ApplicationController
   def index
     @bags = Bag.all
   end
+
   def show
     @bag = Bag.find(params[:id])
   end
+
   def new
     @bag = Bag.new
   end
+
   def create
     @bag = Bag.new(bag_params)
     @bag.user = current_user
@@ -16,26 +19,28 @@ class BagsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-
-    def edit
-      @bag = Bag.find(params[:id])
-      @bag.user = current_user
-      raise
-    end
-
-    def update
-      @bag = Bag.find(params[:id])
-      @bag.update(bag_params)
-      redirect_to bag_path(@bag)
-    end
   end
+
+  def edit
+    @bag = Bag.find(params[:id])
+  end
+
+  def update
+    @bag = Bag.find(params[:id])
+    @bag.update(bag_params)
+    redirect_to bag_path(@bag)
+  end
+
   def destroy
     @bag = Bag.find(params[:id])
     @bag.destroy
     redirect_to profile_path(current_user), status: :see_other
   end
+
   private
+
   def bag_params
     params.require(:bag).permit(:name, :brand, :price, :description, :picture_url, :condition, :location, photos: [])
   end
+
 end
