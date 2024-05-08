c5db893b4003e53459f2f945b81fdee568f9819b
catalin ursu <ursucatalin0@gmail.com>
2024-05-08 12:37:22 +0100

    pictures on index page ...Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 # db/seeds.rb
+require "open-uri"
 require 'faker'
 
 User.destroy_all
@@ -18,12 +19,12 @@ User.destroy_all
      email:"catalin@gmail.com",
     password: "password123"
   )
-
+   bag_links =["https://images-na.ssl-images-amazon.com/images/I/813ptKmjJVL.jpg","https://m.media-amazon.com/images/I/71MQz9WZAxL._AC_SY695_.jpg","https://m.media-amazon.com/images/I/61mcR7xSM0L._AC_SX535_.jpg"]
 # Delete existing bags
 Bag.destroy_all
 
 # Create 20 bags with regular brands
-20.times do
+20.times do |i|
   new_bag = Bag.new(
     name: Faker::Commerce.product_name,
     brand: Faker::Company.name,
@@ -31,10 +32,17 @@ Bag.destroy_all
     description: Faker::Lorem.paragraph(sentence_count: 2),
     condition: Faker::Hipster.word,
     location: Faker::Address.city
-  )
+
+ )
+ file = URI.open(bag_links[i])
+
+ new_bag.photos.attach(io: file, filename: "Katabag.png", content_type: "image/png")
+
+
   new_bag.user = User.first
  new_bag.save
 end
+
 
 
 # Custom picture URLs for designer bags
