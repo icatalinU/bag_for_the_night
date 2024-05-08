627932d35fcb7c4262809d20c6319ffb832dc4b7
catalin ursu <ursucatalin0@gmail.com>
2024-05-08 14:45:21 +0100

    update on index page hoover over picture cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,17 @@
 # db/seeds.rb
 require "open-uri"
 require 'faker'
-require 'open-uri'
-
 User.destroy_all
-
  #=> { username: 'catalin', email: 'catalin@gmail.com', password: 'password123' }
   User.create(
-
      email:"catalin@gmail.com",
     password: "password123"
   )
-
    bag_links =["https://images-na.ssl-images-amazon.com/images/I/813ptKmjJVL.jpg","https://m.media-amazon.com/images/I/71MQz9WZAxL._AC_SY695_.jpg","https://m.media-amazon.com/images/I/61mcR7xSM0L._AC_SX535_.jpg"]
 # Delete existing bags
 Bag.destroy_all
-
 # Create 20 bags with regular brands
 20.times do |i|
-
-# Delete existing bags
-Bag.destroy_all
-
-bag_file = URI.open("https://collection.cloudinary.com/dwsotsv3c/cc34acfedc88f725cbdba817ac196e16")
-20.times do
-
   new_bag = Bag.new(
     name: Faker::Commerce.product_name,
     brand: Faker::Company.name,
@@ -41,20 +28,10 @@ bag_file = URI.open("https://collection.cloudinary.com/dwsotsv3c/cc34acfedc88f72
     description: Faker::Lorem.paragraph(sentence_count: 2),
     condition: Faker::Hipster.word,
     location: Faker::Address.city
-
  )
  file = URI.open(bag_links[i])
-
  new_bag.photos.attach(io: file, filename: "Katabag.png", content_type: "image/png")
-
-  )
-
-  new_bag.photos.attach(io: bag_file, filename: "bag.png", content_type: "image/png")
-
   new_bag.user = User.first
  new_bag.save
 end
-
 puts "Seeding completed!"
-
- 
