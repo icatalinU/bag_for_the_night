75089c0cc2717db4522493e78d914c808e87db36
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-05-08 12:09:32 +0100

    updated navbar with working avatar and links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def profile
     @user = params[:current_user]
-    @bags = Bag.where(:user_id == @user_id)
-  end
-
-  def show
-    @bags = Bag.all
+    @bags = Bag.where(@user_id == :user_id)
+    @bookings = Booking.where(@user == :user_id)
   end
 end
