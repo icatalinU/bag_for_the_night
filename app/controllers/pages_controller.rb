d78b16f547012eea6d29d5c4ec36673e357aaca7
catalin ursu <ursucatalin0@gmail.com>
2024-05-09 15:15:01 +0100

    home-page  buton links for rent and create from home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
     @user = params[:current_user]
     @bags = Bag.where(@user_id == :user_id)
     @bookings = Booking.where(@user == :user_id)
+    
   end
 end
